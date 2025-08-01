ff2cc6059d0309d514a8b8864f1f60a6980a1a88
Joseph Hau <hexart@gmail.com>
2025-08-01 10:37:37 +0800

    Remove I18nextProvider wrapper from main.tsx
    Simplify React app initialization by removing redundant I18nextProvider wrapper since i18n initialization is now handled directly in the i18n.js file. This makes the entry point cleaner while maintaining internationalization support.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,16 +1,13 @@
 import './env';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import './i18n';
 import App from './App';
 import './index.css';
 import './scrollbar.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <App />
   </React.StrictMode>
 );
